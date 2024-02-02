d11d30c72e7899c3fdcde6d94f4925fec6d3860b
nohelbey <158280877+nohelbey@users.noreply.github.com>
2024-02-02 13:31:38 +0100

    Update bateria1.py

--- bateria1.py
+++ bateria1.py
@@ -1,1 +1,8 @@
+# Programa que pregunta dos números a l'usuari i calcula la seva suma.
 
+numero1 = float(input("Introduce el primer número: "))
+numero2 = float(input("Introduce el segundo número: "))
+
+suma = numero1 + numero2
+
+print(f"La suma de {numero1} y {numero2} es: {suma}")
