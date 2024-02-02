c7e0192d4c65c8102176ecb0d9bd317e67984be8
nohelbey <158280877+nohelbey@users.noreply.github.com>
2024-02-02 13:38:01 +0100

    Update bateria1.py

--- bateria1.py
+++ bateria1.py
@@ -6,3 +6,12 @@ numero2 = float(input("Introduce el segundo número: "))
 suma = numero1 + numero2
 
 print(f"La suma de {numero1} y {numero2} es: {suma}")
+
+
+# Programa que pregunta un número i retorna el seu quadrat.
+
+numero = float(input("Introduce un número: "))
+
+cuadrado = numero ** 2
+
+print(f"El cuadrado de {numero} es: {cuadrado}")
